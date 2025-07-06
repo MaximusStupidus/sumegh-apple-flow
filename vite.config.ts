07d142b3ea996926f8eaa6910387596203612114
Ojas Jain <ojasjain@192.168.1.3>
2025-07-06 09:57:25 +0530

    Deploy to custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // 👈 base is '/' when using a custom domain!
   server: {
     host: "::",
     port: 8080,
